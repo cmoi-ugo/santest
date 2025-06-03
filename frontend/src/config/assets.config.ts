ed7077a8f2a0c5c377f546c1dd6b16154243b432
cmoi-ugo <magister18yt@gmail.com>
2025-06-03 10:05:05 +0200

    add app title and logo in topbar

--- frontend/src/config/assets.config.ts
+++ frontend/src/config/assets.config.ts
@@ -1,7 +1,11 @@
+import logo from '@/assets/images/logo.svg';
+import appSvg from '@/assets/images/app-name.svg';
 import defaultQuizImage from '@/assets/images/default-quiz.jpg';
 import addIcon from '@/assets/images/add.svg';
 
 export const ASSETS = {
+  LOGO: logo,
+  APP_NAME: appSvg,
   IMAGES_BASE_URL: import.meta.env.VITE_IMAGES_URL || './src/assets/images',
   DEFAULT_IMAGES: {
     QUIZ: defaultQuizImage,
--- frontend/src/layouts/TopBar/TopBar.tsx
+++ frontend/src/layouts/TopBar/TopBar.tsx
@@ -1,4 +1,6 @@
 import styles from '@/layouts/TopBar/TopBar.module.css';
+import { ASSETS, ROUTES } from '@/config';
+import { NavLink } from 'react-router-dom';
 
 interface TopBarProps {
   railExpanded: boolean;
@@ -9,6 +11,22 @@ export const TopBar: React.FC<TopBarProps> = ({ railExpanded, children }) => {
   return (
     <div className={`${styles.topBar} ${railExpanded ? styles.railExpanded : ''}`}>
       <div className={styles.topBarContent}>
+        <div className={styles.logoContainer}>
+          <NavLink to={ROUTES.HOME} className={styles.logoLink}>
+            <img 
+              src={ASSETS.LOGO} 
+              alt="Logo" 
+              className={styles.logo}
+            />
+          </NavLink>
+          <NavLink to={ROUTES.HOME} className={styles.appNameLink}>
+            <img 
+              src={ASSETS.APP_NAME} 
+              alt="App Name" 
+              className={styles.appName}
+            />
+          </NavLink>
+        </div>
         {children}
       </div>
     </div>
